364f116f0aacf04928ae00ce1912253b65b89f8a
Thomas Mills <mills.thoma@gmail.com>
2024-07-31 12:02:00 +0200

    cloneAndCheckout test added

--- src/git/cloneAndCheckout.test.ts
+++ src/git/cloneAndCheckout.test.ts
@@ -1,18 +1,23 @@
 import { expect, it, describe, vi } from 'vitest';
 import cloneAndCheckout from './cloneAndCheckout';
 
+const { cloneMock } = vi.hoisted(() => {
+  return {
+    cloneMock: vi.fn().mockResolvedValue({
+      cwd: vi.fn().mockReturnThis(),
+    }),
+  };
+});
+
 const { checkoutMock } = vi.hoisted(() => {
-  return { checkoutMock: vi.fn() };
+  return { checkoutMock: vi.fn().mockReturnValue(Promise.resolve()) };
 });
 
 vi.mock('simple-git', async importOriginal => {
   const mockGit = {
     clean: vi.fn().mockReturnThis(),
-    clone: vi.fn().mockResolvedValue({
-      cwd: vi.fn().mockReturnThis(),
-    }),
+    clone: cloneMock,
     cwd: vi.fn().mockReturnThis(),
-    // checkout: vi.fn(),
     checkout: checkoutMock,
   };
 
@@ -32,13 +37,26 @@ vi.mock('simple-git', async importOriginal => {
 });
 
 describe('cloneAndCheckout', () => {
-  it('foobar', async () => {
+  it('clones the requested repo into the correct directory', async () => {
     await cloneAndCheckout(
       'git@github.com:Amsterdam/development-standards.git',
+      'docs/local',
       'feature/foobar'
     );
 
-    expect(checkoutMock).toHaveBeenCalled();
+    expect(cloneMock).toHaveBeenCalledWith(
+      'git@github.com:Amsterdam/development-standards.git',
+      'docs/local'
+    );
+  });
+
+  it('checkouts if the branch is other than main', async () => {
+    await cloneAndCheckout(
+      'git@github.com:Amsterdam/development-standards.git',
+      'docs/local',
+      'feature/foobar'
+    );
+
     expect(checkoutMock).toHaveBeenCalledWith('feature/foobar');
   });
 });
